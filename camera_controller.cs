5363f50079ff047be59c8df2d0f0f9a8959ce224
wijak manoprang <94544396+krit0885824924@users.noreply.github.com>
2021-12-13 19:27:19 +0700

    Add files via upload

---
+++ camera_controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class camera_controller : MonoBehaviour
+{
+    public Transform obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(obj.position.x, transform.position.y, transform.position.z);
+    }
+}
